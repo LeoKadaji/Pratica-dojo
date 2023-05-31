1f981c3aa2386586aac72ce690d4eb7cc52d10fe
LeoKadaji <carlosotoline@gmail.com>
2023-05-31 17:08:33 -0300

    pratica-dojo

--- src/Pacote3/Aluno.java
+++ src/Pacote3/Aluno.java
@@ -1,19 +1,37 @@
 package Pacote3;
-public class Aluno extends Pessoa {
+public class Aluno implements Comparable<Aluno> {
 
-protected int matricula;
+private String nome;
+private int matricula;
 
-    public Aluno(String Nome, String Cpf){
-        this.Nome=Nome;
-        this.Cpf=Cpf;
+public Aluno(String nome, int matricula){
+    this.nome=nome;
+    this.matricula=matricula;
+}
+
+@Override
+public String toString() {
+    return String.format("Nome: %s - Matricula: %s", this.nome, this.matricula);
+}
+
+    @Override
+    public int compareTo(Aluno outroAluno){
+       return this.getNome().compareTo(outroAluno.getNome());
     }
-
+    public String getNome() {
+        return nome;
+    }
+    
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+    
     public int getMatricula() {
         return matricula;
     }
-
+    
     public void setMatricula(int matricula) {
         this.matricula = matricula;
     }
-    
 }
+    
